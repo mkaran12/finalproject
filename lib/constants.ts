4ac5a2ed134d5f057e37bea1c2233c0cc8740b8c
Moses Karani <mk@Mosess-MacBook-Pro.local>
2025-01-23 11:22:54 +0300

    fsetting component

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'AchlaShop'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'do it, do it well, do it fast'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'AchlaShop'
